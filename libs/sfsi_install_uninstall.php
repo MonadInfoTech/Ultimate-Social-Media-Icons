051250cff44bf17f0147854a4563885a41d582d6
Arpan Patel <arpan@monadinfotech.com>
2015-10-29 15:46:47 +0530

    Release 1.3.1
    New Version (With Shortpixel Link Change, Quick Claiming, CSS
    modification )

--- libs/sfsi_install_uninstall.php
+++ libs/sfsi_install_uninstall.php
@@ -12,7 +12,7 @@ function sfsi_update_plugin()
 	}
 	
 	//Install version
-	update_option("sfsi_pluginVersion", "1.3");
+	update_option("sfsi_pluginVersion", "1.31");
 	
 	/*show notification*/
 	if(!get_option('show_notification'))
